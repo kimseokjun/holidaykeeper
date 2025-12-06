f6d4dd3a1d59054a9c00572d1993d58de6ef99b2
kimseokjun <k03s13j@naver.com>
2025-12-06 12:42:23 +0900

    chore: 프로젝트 초기 설정 및 의존성 구성

---
+++ src/test/java/org/example/holidaykepper/HolidaykepperApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.holidaykepper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidaykepperApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
